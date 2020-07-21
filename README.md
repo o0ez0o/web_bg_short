681ac128c85913c1eb0fa9caad2574cafc73071e
Yi Liu <31673018+o0ez0o@users.noreply.github.com>
2020-07-21 12:32:00 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+
+Visit the website view: https://o0ez0o.github.io/2DMiniGame_toFIND_p5js/
---
+++ sketch.js
@@ -0,0 +1,134 @@
+// All the paths
+let paths = [];
+// Are we painting?
+let painting = false;
+// How long until the next circle
+let next = 0;
+// Where are we now and where were we?
+let current;
+let previous;
+ 
+function setup() {
+  createCanvas(2500, 3000);
+  current = createVector(0,0);
+  previous = createVector(0,0);
+};
+ 
+function draw() {
+  background(255);
+  // If it's time for a new point
+
+  if (millis() > next && painting) {
+ 
+    // Grab mouse position      
+    current.x = mouseX;
+    current.y = mouseY;
+ 
+    // New particle's force is based on mouse movement
+    let force = p5.Vector.sub(current, previous);
+    force.mult(0.1);
+ 
+    // Add new particle
+    paths[paths.length - 1].add(current, force);
+    
+    // Schedule next circle
+    next = millis() + random(80);
+ 
+    // Store mouse values
+    previous.x = current.x;
+    previous.y = current.y;
+  } else if (!painting) {
+    startDraw();
+  }
+  
+ 
+  // Draw all paths
+  for( let i = 0; i < paths.length; i++) {
+    paths[i].update();
+    paths[i].display();
+  }
+}
+function startDraw() {
+  next = 0;
+  painting = true;
+  previous.x = mouseX;
+  previous.y = mouseY;
+  paths.push(new Path());
+}
+ 
+// Start it up
+// function mousePressed() {
+//   startDraw();
+// }
+ 
+// Stop
+// function mouseReleased() {
+//   painting = false;
+// }
+ 
+// A Path is a list of particles
+class Path {
+  constructor() {
+    this.particles = [];
+    this.hue = random(100);
+  }
+ 
+  add(position, force) {
+    // Add a new particle with a position, force, and hue
+    this.particles.push(new Particle(position, force, this.hue));
+  }
+  
+  // Display plath
+  update() {  
+    for (let i = 0; i < this.particles.length; i++) {
+      this.particles[i].update();
+    }
+  }  
+  
+  // Display plath
+  display() {    
+    // Loop through backwards
+    for (let i = this.particles.length - 1; i >= 0; i--) {
+      // If we shold remove it
+      if (this.particles[i].lifespan <= 0) {
+        this.particles.splice(i, 5);
+      // Otherwise, display it
+      } else {
+        this.particles[i].display(this.particles[i+1]);
+      }
+    }
+  
+  }  
+}
+ 
+// Particles along the path
+class Particle {
+  constructor(position, force, hue) {
+    this.position = createVector(position.x, position.y);
+    this.velocity = createVector(force.x, force.y);
+    this.drag = 0.96;
+    this.lifespan = 300;
+    this.d = Math.round(Math.random()*20+5);;;
+  }
+ 
+  update() {
+    // Move it
+    this.position.add(this.velocity);
+    // Slow it down
+    this.velocity.mult(this.drag);
+    // Fade it out
+    this.lifespan--;
+  }
+ 
+  // Draw particle and connect it with a line
+  // Draw a line to another
+  display(other) {
+    stroke(50, this.lifespan);
+    fill(100, this.lifespan/2);    
+    ellipse(this.position.x,this.position.y, this.d, this.d);    
+    // If we need to draw a line
+    if (other) {
+      line(this.position.x, this.position.y, other.position.x, other.position.y);
+    }
+  }
+}
